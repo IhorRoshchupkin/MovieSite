6e5886a0768a5f3cb3623b2458674ee58780a037
inf150522 <mykola.muts@student.put.poznan.pl>
2023-03-28 14:05:09 +0200

    minor changes in add_rating.php

--- add_rating.php
+++ add_rating.php
@@ -4,7 +4,11 @@
   if (isset($_POST['submit_rating'])) {
     $movie_id = $_POST['movie_id'];
     $rating_value = $_POST['rating'];
-    $user_id = 1; // replace with the actual user ID
+    if (isset($_COOKIE["userID"])) {
+        $user_id = $_COOKIE["userID"];
+      } else {
+        $user_id = 1;
+      }
 
     add_rating($movie_id, $user_id, $rating_value);
 
